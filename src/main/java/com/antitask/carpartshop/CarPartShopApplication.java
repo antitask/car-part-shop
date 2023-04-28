d4aae353c2d5e0d9047cc7870b457c45d1ab4271
antitask <admin@antitask.com>
2023-04-28 17:14:36 +0200

    CPS - refactor main class name (app name)

--- src/main/java/com/antitask/carpartshop/CarPartShopApplication.java
+++ src/main/java/com/antitask/carpartshop/CarPartShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarpartshopApplication {
+public class CarPartShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarpartshopApplication.class, args);
+		SpringApplication.run(CarPartShopApplication.class, args);
 	}
 
 }
